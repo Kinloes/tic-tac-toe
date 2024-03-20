80dcd0715feedb3f8a8ad24b74dd418ed0eb0802
SlipStreme <Kinloes@gmail.com>
2024-03-19 22:27:00 -0400

    Added base code that allows the player to input info and a click listener to start game.

--- ttt.js
+++ ttt.js
@@ -1,1 +1,15 @@
-console.log("JS Linked")
+playerInput()
+function playerInput(){
+    const playerArray = [];
+    let name = prompt("What is your name?")
+    let marker = prompt("Set your marker to X or O")
+    console.log("Hello, " + name + "! Your marker is " + marker)
+    this.name = name;
+    this.marker = marker;
+    let arrayAdd = playerArray.push(name + " " + marker)
+    console.log (playerArray)
+}
+
+function startGame(){
+    alert("it works")
+}
