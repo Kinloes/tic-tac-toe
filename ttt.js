f91afa0ecda23789d8e11da0d6425fb51c095cf8
SlipStreme <Kinloes@gmail.com>
2024-03-26 21:13:00 -0400

    Added code that allows the user to choose and place their marker. Figuring out the best way to ensure the user only picks either "x" or "o"

--- ttt.js
+++ ttt.js
@@ -1,8 +1,20 @@
-playerInput()
-function playerInput(){
+//playerInput()
+function startGame(){
     const playerArray = [];
     let name = prompt("What is your name?")
+    do{
     let marker = prompt("Set your marker to X or O")
+    if (((marker) === "o") || ((marker) === "O")){
+        marker = "O";
+           }       else if (((marker) === "x") || ((marker) === "X")){
+                marker = "X"
+        } else{
+            alert("Please enter either X or O");
+            console.log(marker);
+        }
+    }
+    while ((this.marker) != ("X" || "O"))
+    console.log(this.marker);
     console.log("Hello, " + name + "! Your marker is " + marker)
     this.name = name;
     this.marker = marker;
@@ -11,7 +23,10 @@ function playerInput(){
 }
 
 function TESTALERT(clicked_id){
-    //const realtest = document.getElementById(this.id);
-    //console.log(realtest);
-    alert(clicked_id);
+    console.log(clicked_id);
+    let playerMark = document.getElementById(clicked_id);
+    console.log(playerMark);
+    playerMark.innerText = marker;
+    //alert(clicked_id);
+
 }
